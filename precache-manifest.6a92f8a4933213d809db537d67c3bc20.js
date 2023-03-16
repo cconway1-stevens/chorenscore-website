480262e2ccc54597c0c3d85fc53031db99df028d
cconway1-stevens <55499408+cconway1-stevens@users.noreply.github.com>
2023-03-16 19:31:13 -0400

    send

---
+++ precache-manifest.6a92f8a4933213d809db537d67c3bc20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9bf7851e6a86c00d37e61653467c04",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4e726533467da5d61ef6",
+    "url": "/portfolio/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "724f70080248f184b896",
+    "url": "/portfolio/static/js/2.f5bb07c4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.f5bb07c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e726533467da5d61ef6",
+    "url": "/portfolio/static/js/main.72eb5358.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
